e13e4b5bf10c924c83fbf35f614b3be89fd24ffe
Oyerinde Enoch <oyerindeenoch@gmail.com>
2022-07-19 18:43:39 +0100

    Add ANDROID AND WEB DEPENDENCIES

---
+++ beia-hq-telemetry/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.enoch.beiahq',
+  appName: 'BeiaHQ Telemetry',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
